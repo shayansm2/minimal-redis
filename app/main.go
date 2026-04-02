073fe15acd736731619bb0173a1d25b91054a967
Shayan ShafieeMoghadam <shayan_shafieemoghadam@hamravesh.com>
2026-04-02 15:49:03 +0330

    handle multiple connections and multiple command in the same connection

--- app/main.go
+++ app/main.go
@@ -6,27 +6,33 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment the code below to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer listener.Close()
 
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		in := make([]byte, 4096)
+		_, err := conn.Read(in)
+		if err != nil {
+			conn.Close()
+		}
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
--- tests/README.md
+++ tests/README.md
@@ -6,3 +6,10 @@ nc localhost 6379
 ```
 
 3.  Respond to multiple PINGs:
+4.  Handle concurrent clients:
+
+```shell
+redis-cli PING
+echo -e "PING\nPING" | redis-cli
+nc localhost 6379
+```
